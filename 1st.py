fe9f89f51a40e12b147f8d84cd24451f9ba86962
priyanshukaushikgk <priyanshukaushikgk@gmail.com>
2024-06-06 09:55:59 +0530

    first python commit

---
+++ 1st.py
@@ -0,0 +1,14 @@
+# print("hello python")
+# in python same memory address m change is called  mutable or not is called immutable.
+
+
+# age  = 18
+# print(id(age))
+
+# salary =age
+# print(id(salary))
+
+# salary = 20
+# print("new",id(salary))
+
+
---
+++ assigmment1.py
@@ -0,0 +1,73 @@
+#question - 1
+# n1= 2
+# n2= 5
+# n3= 4
+# if(n1>n2 and n1>n3):
+#     print("n1 is greater")
+# elif(n2>n1 and n2>n3):
+#     print("n2 is greater")
+# else:
+#     print("n3 is grater then n1 and n2")
+
+#question - 2  PRINT WEEKS DAYS
+
+# num = int(input("Enter the day number:"))
+# if(num == 1):
+#     print("sunday")
+# elif(num == 2):
+#     print("monday")
+# elif(num == 3):
+#     print("tuesday")
+# elif(num == 4):
+#     print("wednesday")
+# elif(num == 5):
+#     print("thrusday")
+# elif(num == 6):
+#     print("friday")
+# elif(num == 7):
+#     print("saturday")
+# else:
+#     print("Enter right Digit")
+
+
+#Question - 3 PRINT MONTHS NAME
+
+
+# num = int(input("Enter the number:"))
+# if(num==1):
+#     print("january")
+# elif(num==2):
+#     print("febuarary")
+
+# elif(num==3):
+#     print("march")
+
+# elif(num==4):
+#     print("april")
+
+# elif(num==5):
+#     print("may")
+
+# elif(num==6):
+#     print("june")
+
+# elif(num==7):
+#     print("july")
+
+# elif(num==8):
+#     print("auguest")
+
+# elif(num==9):
+#     print("september")
+
+# elif(num==10):
+#     print("octuber")
+
+# elif(num==11):
+#     print("november")
+
+# elif(num==12):
+#     print("December")
+# else:
+#     print("Enter correct Digit")
+
