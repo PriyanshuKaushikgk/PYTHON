ce563f21a5d29aa2cf9f413d4b0ce3c274cb71a2
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-06-21 11:51:08 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,16 @@
+x = 10
+print(id(x))
+
+y=x
+print(id(y))
+
+age= 18
+print(id(age))
+
+# id is use for memory address.
+salary=age
+print(id(salary))
+salary=20
+print("New",id(salary))
+
+print(id(age))
---
+++ list.py
@@ -0,0 +1,34 @@
+# mylist = [1.0,10,20,60.0,5,30]
+# newlist = []
+
+# for i in mylist:
+#     if (type(i) is int):
+#         newlist.append(i**2)
+
+#         print(newlist)
+
+
+# mylist = [1.0,10,20,60.0,5,30]
+# for i in mylist:
+#     if(type(i) is int):
+#         print(i)
+
+
+# mylist = ["hello","hey", "tushar", "Regex"]
+# newlist=[]
+# for i in mylist:
+#     newlist.append(len(i))
+# print(newlist)
+
+
+# TUPLE
+# IMMUTABLE
+#MULTIPLE DATATYPEELEMENT
+# INDEX POSITION
+#FASTER
+
+mytuple = 10
+print(type(mytuple))
+
+mytuple = 10,20,"hey"
+print(type(mytuple))
