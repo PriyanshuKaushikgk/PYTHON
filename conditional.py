b27d3fcc5cf6cc72510b9faaec729b895c5f920c
PriyanshuKaushikgk <161697370+PriyanshuKaushikgk@users.noreply.github.com>
2024-06-25 11:59:39 +0530

    Add files via upload

--- conditional.py
+++ conditional.py
@@ -34,12 +34,65 @@
 #     print("odd")
 
 
-num  = 84
-if(num>=10):
-    print("num is greater then 10")
-    if(num%5==0):
-        print("num is also divisible by 5")
-    else:
-        print("num is not divisible by 5")
-else:
-    print("num is not greater then or equal to 10")
+# num  = 84
+# if(num>=10):
+#     print("num is greater then 10")
+#     if(num%5==0):
+#         print("num is also divisible by 5")
+#     else:
+#         print("num is not divisible by 5")
+# else:
+#     print("num is not greater then or equal to 10")
+
+#swap number:--------->>>>>>>>>>>
+# a = 5
+# b = 3
+# print("before swap:")
+# print(a)
+# print(b)
+
+# a ,b = b,a
+# print("after swap:")
+# print(a)
+# print(b)
+
+
+
+# #Reverse number programm --------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# num = int(input("Enter a number: "))
+
+# reversed_num = 0
+# while num != 0:
+#   digit = num % 10
+#   reversed_num = reversed_num * 10 + digit
+#   num //= 10
+
+# print("The reversed number is:", reversed_num)
+
+
+
+#sum of three digits ---------->>>>>>>>>>
+
+number = 123
+first_digit = number//100
+seecond_digit = (number%100)//10
+third_digit = number%10
+sumofdigits = first_digit+seecond_digit+third_digit
+square  = sumofdigits**2
+print(square)
+
+
+
+
+# number = 123
+# first_digit = number//100
+# seecond_digit = (number%100)//10
+# third_digit = number%10
+# sumofdigits = first_digit*first_digit+seecond_digit*seecond_digit+third_digit*third_digit
+# print("the square is:", sumofdigits)
+
+
+
+
+
+
